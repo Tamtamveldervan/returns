1e8101f1745d3f8a7f282d99b2276328756b6620
Tamtamveldervan <84381450+Tamtamveldervan@users.noreply.github.com>
2021-06-23 16:05:59 +0200

    Add files via upload

---
+++ return.js
@@ -0,0 +1,54 @@
+
+function checkNumber(number)
+{ 
+    if (number >100){
+    return true}
+   else (number <=100)
+
+{return false } }
+
+console.log (checkNumber(99))
+
+//this function produces something
+
+
+/* Brenda the bouncer bot
+
+"come in"
+"it's too busy now, come back later"
+"this is a club for adults"
+
+max hoeveelheid in de club: 500
+min leeftijd: 18 */
+
+
+
+function entranceCheck(age, amount)
+{if (age <18) return ("this is a club for adults");
+else if (amount >=500) return ("it's too busy now, come back later");
+else if (age >=18, amount <500) return ("come in");
+}
+
+console.log (entranceCheck (19, 250))
+//this function produces something
+/*de enige andere manier waarop ik dit kon opschrijven, wat ik kon bedenken, was twee verschillende functions
+eentje met age en eentje met amount, maar dat werkte minder goed. */
+
+/*const calculateAverage=function(numbers){
+    const average=numbers.reduce((total,n)=> total+n)/numbers.length;
+    console.log(average);
+};*/
+
+calculateAverage([6,8,9,7,3]);
+
+const calculateAverage=numbers (nrA, nrB, nrC, nrD, nrE)
+{{
+((5+6+8+3+9)
+}
+};
+
+
+//dit zal wel niet goed zijn, maar geen idee hoe het anders moet - ik kwam allerlei manieren tegen :/
+//ook deze functie produces iets. 
+
+
